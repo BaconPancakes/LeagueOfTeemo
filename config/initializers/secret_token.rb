94c6b5061841767b0ccc5a3c1c00a674e7074e40
Allen Qin <me2myself3andi@gmail.com>
2013-12-25 16:38:27 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teemo::Application.config.secret_token = '31e9e7c748d409cda7233d731fc5b1737bd6869c48e8d16a686509cda1bddf055be3ccc4a033d22454bd4f9e1dc126b52793a198a22d2e62380455b96b5c3a24'
